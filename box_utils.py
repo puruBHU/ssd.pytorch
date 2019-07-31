3032b91f39202298f1efab9a514a1e4b13f439b9
puruBHU <purnendu06@gmail.com>
2019-07-31 20:31:47 +0530

    new

--- box_utils.py
+++ box_utils.py
@@ -39,6 +39,8 @@ def intersect(box_a, box_b):
     """
     A = box_a.size(0)
     B = box_b.size(0)
+    
+    print(A, B)
     max_xy = torch.min(box_a[:, 2:].unsqueeze(1).expand(A, B, 2),
                        box_b[:, 2:].unsqueeze(0).expand(A, B, 2))
     min_xy = torch.max(box_a[:, :2].unsqueeze(1).expand(A, B, 2),
